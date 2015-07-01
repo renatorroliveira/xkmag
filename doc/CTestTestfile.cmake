0311504cd8b6030c4385a2d6b41acb75c4dcd94a
Renato R. R. de Oliveira <renatorro@comp.ufla.br>
2015-06-30 21:04:57 -0300

    Updating to KDE 4.14 codebase

--- doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/renato/DADOS/Linux/Programming/kmag-4.12.3/doc
-# Build directory: /media/renato/DADOS/Linux/Programming/kmag-4.12.3/doc
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
